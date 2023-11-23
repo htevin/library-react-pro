b740f63202f9412681310c52e0ff3197561ad9b6
Tevin Hamilton <tevinhamilton@yahoo.com>
2023-11-22 20:20:01 -0500

    chnage #6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { books } from "./data";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookInfo from "./Pages/BookInfo";
 import Cart from "./Pages/Cart";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -46,8 +46,16 @@ function App() {
       <div className="App">
         <Nav numberOfItems={numberOfItems()} />
         <Route path="/" exact component={Home} />
-        <Route path='/library-react-pro' component={Home}/>
-        <Route path="/books" exact render={() => <Books books={books} />} />
+
+
+        <Route path='https://htevin.github.io/library-react-pro' exact component={Home}/>
+        <Route path='https://htevin.github.io/library-react-pro/' exact component={Home}/>
+        <Route path='https://htevin.github.io/library-react-pro' component={Home}/>
+        <Route path='/library-react-pro' exact component={Home}/>
+
+
+
+        <Route path="/books" render={() => <Books books={books} />} />
         <Route
           path="/books/:id"
           render={() => <BookInfo books={books} addToCart={addToCart} cart={cart}/>}
